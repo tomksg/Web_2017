4b0679adf12da915ba24bdd413af6689a39618fd
tomksg <tomksg@naver.com>
2017-12-13 20:22:29 +0900

    http://gakari.tistory.com/entry/PHP-MYSQL과-PHP를-이용한-간단한-게시판-만들기

---
+++ php/php_FormForProject/db_info.php
@@ -0,0 +1,23 @@
+<!-- php 용 mysql 접근 게시판 데이터베이스 -->
+
+// <?
+    $conn = mysql_connect("localhost", "", ""); //접속 ip , 아이디 , 패스워드
+    mysql_select_db("",$conn);	//디비이름
+
+
+
+// 출처: http://gakari.tistory.com/entry/PHP-MYSQL과-PHP를-이용한-간단한-게시판-만들기 [가카리의 공부방]
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
