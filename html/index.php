ff62bec887b301bba701c547057e2296548003fd
Juwon <feelgoodinc74@gmail.com>
2017-12-13 13:44:20 +0900

    2017.12.13 1:43 pm
    기본뼈대만 구축되어있고 페이지끼리 링크없음

---
+++ html/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
+    <title>hi 건어물집에 오신걸 환영합니다</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/box_design.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+      <div class="navbar-wrapper">
+        <div class="container">
+
+          <nav class="navbar navbar-static-top">
+            <div class="container">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar justified"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+        </div>
+              <form class="navbar-form navbar-right" role="search">
+                   <div class="form-group">
+                     <input type="text" class="form-control" placeholder="Search">
+                   </div>
+                   <button type="submit" class="btn btn-default" onclick="javascript:location.href='localhost/shop/list.php'">Submit</button>
+                 </form>
+            </div>
+          </nav>
+        </div>
+      </div>
+  <div class="bs-example" data-example-id="embedded-scrollspy">
+  <div class="collapse navbar-collapse bs-example-js-navbar-scrollspy">
+    <ul id="ex1" class="nav nav-pills nav-justified">
+    <li id="info" role="presentation" class="active"><a href="#0">소개</a></li>
+      <li id="list" role="presentation"><a href="#1">상품목록</a></li>
+      <li id="lo" role="presentation"><a href="#2">오시는 길</a></li>
+    </ul>
+  </div>
+
+    <style>
+    body { padding-bottom: 70px; }
+    </style>
+    <div class="vertical">
+    <span class="vertical">
+    건어물집 소개
+    </span>
+  </div>
+      <div data-spy="scroll" data-target="ex1" data-offset="0" class="scrollspy-example">
+    <div id="info" class="jumbotron">
+       <div class="container">
+     <div class="row featurette">
+            <div class="col-md-7 col-md-push-5">
+              <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5 col-md-pull-7">
+              <img class="featurette-image img-responsive center-block" src="img/squid.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            </div>
+          </div>
+    </div>
+    </div>
+      <hr class="featurette-divider">
+
+    <div id="list" class="container">
+    <div class="row">
+          <div class="col-lg-3">
+            <a href="<?php
+            echo "list.php?id=";
+            $temp = 2;
+            echo $temp;
+            ?>" class="thumbnail">
+            <img src="img/squid.jpg" alt="..."></a>
+<!-- 여기서 디비에서 값이랑 아이디 가져오는거 추가하고 가져오면서 img 소스에 들어갈  -->
+<!-- php 변수를 위에서 미리 정의해주자  -->
+         </div>
+          <div class="col-lg-3">
+            <a href="#" class="thumbnail">
+          <img src="img/squid.jpg" alt="..."></a>
+           </div>
+          <div class="col-lg-3">
+            <a href="#" class="thumbnail">
+          <img src="img/squid.jpg" alt="..."></a>
+          </div>
+        </div>
+    </div>
+     <hr class="featurette-divider">
+     <div id="lo" class="container">
+    <div class="row featurette">
+          <div class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5 col-md-pull-7">
+            <img class="featurette-image img-responsive center-block" src="img/squid.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+    </div>
+    <hr class="featurette-divider">
+
+
+    <footer>
+      <p>&copy; 대표:건어물 | 사업자등록번호 : 123-45-67890
+      </p>
+      <p>
+        대표번호:02-333-3333 | 메일 : driedfood@naver.com
+      </p>
+      <p>
+        copyright (c)seafoodworld.com all rights reserved</p>
+      </footer>
+    </div>
+
+  </div>
+    <ul class="nav nav-pills nav-justified">
+        <li role="presentation"><a href="#">관리자모드</a></li>
+    </ul>
+
+
+
+
+</body>
+</html>
---
+++ html/index_backup.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
+    <title>hi 건어물집에 오신걸 환영합니다</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/box_design.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<!-- 가게 소개 -->
+<div class="vertical">
+  <span class="vertical">
+  건어물집 소개
+  </span>
+</div>
+<div class="row">
+  <div class="col-lg-3" id=right>
+    <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+    <h2>분류1</h2>
+  </div>
+  <div class="col-lg-3">
+    <h5>남들은 잘 모르지만 실수많고 어리버리해 넌내가좋다하지만 알고보면 확깨면 어떡해 사실 까칠하고
+      가끔 예민하고 음식도 조금 가리고</h5>
+  </div>
+</div>
+  <hr width="800">
+  <!-- 상품분류 선택이미지,페이지 누르면 링크로 redirect되게해야함 -->
+      <dl>
+
+       <div class="row" >
+         <div class="col-lg-2" id=right>
+           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+         <div class="col-lg-2" id=right>
+           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+         <div class="col-lg-2">
+         <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+       </div>
+       <div class="row" id=right>
+         <div class="col-lg-2" id=right>
+           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+         <div class="col-lg-2" id=right>
+           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+         <div class="col-lg-2">
+           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         </div>
+       </div>
+     </dl>
+<!-- 제일밑 지도와 대표전화 써놓는 곳 -->
+<hr width="800">
+<div class="row">
+  <div class="col-lg-3" id=right>
+    <img src="img/map.png" alt="Generic placeholder image" width="250" height="250">
+    <h2>분류1</h2>
+  </div>
+  <div class="col-lg-1">
+    <img src="img/info.png" alt="Generic placeholder image" width="250" height="250">
+    <h2>분류2</h2>
+  </div>
+</div>
+
+</body>
+</html>
