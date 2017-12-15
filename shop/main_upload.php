98ee0514f70eefd58e0f26356bfe89959e81519b
KSG <tomksg@naver.com>
2017-12-15 17:29:58 +0900

    파일 업로드

--- shop/main_upload.php
+++ shop/main_upload.php
@@ -1,5 +1,56 @@
 <?php
 
+include("config_getdata.php");
+echo "confirm file information <br />";
+
+  // 이전화면에서 이름이 입력되지 않았으면 "이름을 입력하세요" 메시지 출력
+if(!$_POST['content']) {
+    echo("
+      <script>
+      window.alert('소개를 입력하세요')
+      history.go(-1)
+      </script>
+      ");
+    exit;
+}else{
+    $content1 = $_POST['content'];
+}
+// echo $_POST['content'];
+if(isset($_FILES['upload']["name"])&&(!empty($_FILES["upload"]))){
+    $name = $_FILES['upload']["name"];
+    $tmp_name = $_FILES['upload']['tmp_name'];
+    $Error = $_FILES['upload']['error'];
+    $uploadfile = $_FILES["upload"]["name"];
+}
+
+print_r($_FILES);
+$query = "SELECT number from main WHERE number =1";
+$result = mysqli_query($bd,$query) or die ("Error Querying database.");
+$row = mysqli_fetch_assoc($result);
+
+$id = $row['number'];
+ // // 서버 임시 디렉토리 c:\apm_setup\temp
+
+if(move_uploaded_file($tmp_name,$uploadfile)){
+
+  // upload 는 -> name 으로 지정되어있다. upload php 로 sumbit 기능으로 받을 파일
+   // tmp_name 은 이 파일의 temp 이름으로 다시 지정
+
+   echo "파일이 업로드 되었습니다.<br />";
+   echo "<img src ={$_FILES['upload']['name']}> <p>";
+   echo "1. file name : {$_FILES['upload']['name']}<br />";
+   echo "4. temporary file name : {$_FILES['upload']['tmp_name']}<br />";
+
+
+  $query = "UPDATE main SET image='$uploadfile', description='$content1' WHERE id=$id";//업데이트 쿼리문
+  $result=mysqli_query( $bd,$query);
+  mysqli_close($bd);
+} else {
+   echo "파일 업로드 실패 !! 다시 시도해주세요.<br />";
+}
+
+ //  echo "2. file type : {$_FILES['upload']['type']}<br />";
+ //  echo "3. file size : {$_FILES['upload']['size']} byte <br />";
 /*
 $myusername=addslashes($_POST['id']);
 $mypassword=addslashes($_POST['pw']);
@@ -21,35 +72,5 @@ die(mysqli_error($bd));
 //   </form>
 // </body>
 // </html>
-
-include("config_getdata.php");
-echo "confirm file information <br />";
-$content = $_POST[content];
-$uploadfile = $_FILES['upload'] ['name'];
-$query = "SELECT number from main WHERE number =1";
-$result = mysqli_query($bd,$query) or die ("Error Querying database.");
-$row = mysqli_fetch_assoc($result);
-$id = $row['number'];
-
- // // 서버 임시 디렉토리 c:\apm_setup\temp
-
- if(move_uploaded_file($_FILES['upload']['tmp_name'],$uploadfile)){
-
-  // upload 는 -> name 으로 지정되어있다. upload php 로 sumbit 기능으로 받을 파일
-   // tmp_name 은 이 파일의 temp 이름으로 다시 지정
-
-   echo "파일이 업로드 되었습니다.<br />";
-   echo "<img src ={$_FILES['upload']['name']}> <p>";
-   echo "1. file name : {$_FILES['upload']['name']}<br />";
- //  echo "2. file type : {$_FILES['upload']['type']}<br />";
- //  echo "3. file size : {$_FILES['upload']['size']} byte <br />";
-  echo "4. temporary file name : {$_FILES['upload']['tmp_name']}<br />";
-
-
-  $query = "UPDATE main SET image='$uploadfile', description='$content' WHERE id=$id";//업데이트 쿼리문
-        $result=mysql_query($query, $conn);
- } else {
-   echo "파일 업로드 실패 !! 다시 시도해주세요.<br />";
-}
 
 ?>
--- shop/manage_main.php
+++ shop/manage_main.php
@@ -3,10 +3,10 @@
 include('lock.php');
 include("config_getdata.php");
 
-$query = "SELECT image from main WHERE number =1";
+$query = "SELECT * from main WHERE number =1";
 //$query2 = "SELECT description from main WHERE number=1";
 $result = mysqli_query($bd,$query) or die ("Error Querying database.");
-
+// $result2 = mysql_query($bd,$query2)
 $row = mysqli_fetch_assoc($result);
 
 $src = $row['image'];
@@ -14,9 +14,9 @@ $src2 = $row['description'];
 
 ?>
 
- <!DOCTYPE html>
- <html lang="kor">
- <head>
+<!DOCTYPE html>
+<html lang="kor">
+<head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <!-- 반응형웹으로 만들어주는 태그 -->
@@ -38,26 +38,29 @@ $src2 = $row['description'];
         <caption> 글쓰기 </caption>
       </thead>
       <tbody>
-        <form action="main_upload.php" method="post" encType="multiplart/form-data">
+        <form action="main_upload.php" method="POST" encType="multipart/form-data">
           <tr>
             <th> 홈 이미지 </th>
 
             <!--  서버에있는 이미지파일 가져오고 새거 서버에첨부가능하게 -->
 
-            <td><input type="file" placeholder=" " name="upload" class="form-control"/></td>
-
-    <!-- 여기서 name upload 가 이 파일의 temp 이름이다 -->
-    <!-- <input type="submit" value="send"> -->
+            <td><input type="file" name="upload"/></td>
+            <!-- class="form-control" -->
+            <!-- 여기서 name upload 가 이 파일의 temp 이름이다 -->
+            <!-- <input type="submit" value="send"> -->
 
           </tr>
           <tr>
             <th>소개</th>
-            <td><textarea cols="10"  name="content" class="form-control"><?=$src2?></textarea></td>
+            <td>
+              <textarea cols="10"  name="content" class="form-control"><?=$src2?>
+              </textarea>
+            </td>
           </tr>
           <tr>
             <td colspan="2">
-              <input type="sumbit" value="저장" class="pull-right"/>
-              <input type="button" value="reset" class="pull-left"/>
+              <input type="submit" value="send" class="pull-right"/>
+              <!-- <input type="button" value="reset" class="pull-left"/> -->
               <!-- 리셋시 원래 서버에 저장되어있는거 업로드 -->
               <input type="button" value="뒤로.. " class="pull-right" onclick="history.back(-1)"/>
                     <!-- <a class="btn btn-default" onclick="sendData()"> 등록 </a>
