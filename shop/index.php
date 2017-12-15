f1a6b796875a37878f449b3331c866747386dc28
KSG <tomksg@naver.com>
2017-12-15 21:34:02 +0900

    Index에 소개글 사진 삽입
    manage_list 삽입버튼의 부재

--- shop/index.php
+++ shop/index.php
@@ -1,199 +1,216 @@
+<?php
+include("config_getdata.php");
+
+$query = "SELECT * from main WHERE number =1";
+//$query2 = "SELECT description from main WHERE number=1";
+$result = mysqli_query($bd,$query) or die ("Error Querying database.");
+// $result2 = mysql_query($bd,$query2)
+$row = mysqli_fetch_assoc($result);
+
+$src = $row['image'];
+$src2 = $row['description'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="ko">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
-    <title>hi 건어물집에 오신걸 환영합니다</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
+  <title>hi 건어물집에 오신걸 환영합니다</title>
   <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/box_design.css">
+  <link rel="stylesheet" href="css/box_design.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="C:\Users\user\Desktop\jquery-scroll-follow\lib/jquery.scrollfollow.js"></script>
 
-<style>
+  <style>
 
-#quick {
-  position: absolute;
-  right: 0;
-  top: 100px;
-  height: 200px;
-  background: #black;
-  z-index: 2;
-}
+  #quick {
+    position: absolute;
+    right: 0;
+    top: 100px;
+    height: 200px;
+    background: #black;
+    z-index: 2;
+  }
 
 
 }
 </style>
 </head>
 <script type="text/javascript">
-$(function() {
+  $(function() {
 
-  var UI = {
-    init: function() {
-      this.quickMenuFn();
-      this.topBtn();
-    },
+    var UI = {
+      init: function() {
+        this.quickMenuFn();
+        this.topBtn();
+      },
 
-    initialize: function() {
-      this.id = {
-        target: {
-          quick: '#quick',
-          stickyTop: '#footer'
-        },
-        topBtnClass: 'btn_top'
-      };
-      this.init();
-    },
+      initialize: function() {
+        this.id = {
+          target: {
+            quick: '#quick',
+            stickyTop: '#footer'
+          },
+          topBtnClass: 'btn_top'
+        };
+        this.init();
+      },
 
-    quickMenuFn: function() {
-      var quick = $(this.id.target.quick);
-      var qTop = parseInt(quick.css('top'));
+      quickMenuFn: function() {
+        var quick = $(this.id.target.quick);
+        var qTop = parseInt(quick.css('top'));
 
-      $(window).scroll(function() {
-        var winTop = $(window).scrollTop();
+        $(window).scroll(function() {
+          var winTop = $(window).scrollTop();
 
-        quick.stop().animate({
-          top: winTop + qTop
-        }, 400);
+          quick.stop().animate({
+            top: winTop + qTop
+          }, 400);
 
-      })
-    },
+        })
+      },
 
-    topBtn: function() {
-      var btnLocation = $('.' + this.id.topBtnClass);
-      var timerId = 0;
+      topBtn: function() {
+        var btnLocation = $('.' + this.id.topBtnClass);
+        var timerId = 0;
 
-      $(window).on('scroll', function() {
-        var winTop = $(window).scrollTop();
-        if (winTop > 200) {
-          btnLocation.fadeIn();
-          clearInterval(timerId);
-          timerId = setInterval(btnEffet, 2000);
-        } else {
-          btnLocation.fadeOut();
-          clearInterval(timerId);
+        $(window).on('scroll', function() {
+          var winTop = $(window).scrollTop();
+          if (winTop > 200) {
+            btnLocation.fadeIn();
+            clearInterval(timerId);
+            timerId = setInterval(btnEffet, 2000);
+          } else {
+            btnLocation.fadeOut();
+            clearInterval(timerId);
+          }
+
+        });
+
+        function btnEffet() {
+          btnLocation.fadeTo('300', 0.3).fadeTo('300', 1);
         }
 
-      });
+        this.scrollTop(btnLocation);
+      },
 
-      function btnEffet() {
-        btnLocation.fadeTo('300', 0.3).fadeTo('300', 1);
+      scrollTop: function(eTarget, speed) {
+        var speed = speed || null;
+        eTarget.on('click', function() {
+          $('html, body').animate({
+            scrollTop: $("body").offset().top
+          }, speed)
+        })
       }
 
-      this.scrollTop(btnLocation);
-    },
+    };
 
-    scrollTop: function(eTarget, speed) {
-      var speed = speed || null;
-      eTarget.on('click', function() {
-        $('html, body').animate({
-          scrollTop: $("body").offset().top
-        }, speed)
-      })
-    }
+    $(function() {
+      UI.initialize();
+    })
 
-  };
-
-  $(function() {
-    UI.initialize();
   })
-
-})
 //여기까지 스크롤따라다니는 js
 
 
 </script>
 
 <body>
-    <div id="quick">스크롤 퀵 메뉴</div>
+  <div id="quick">스크롤 퀵 메뉴</div>
 
 
 
-      <div class="navbar-wrapper">
-            <div class="container">
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar justified"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  </div>
-                </button>
-              <form class="navbar-form navbar-right" role="search">
-                   <div class="form-group">
-                     <input type="text" class="form-control" placeholder="Search">
-                   </div>
-                   <button type="submit" class="btn btn-default" onclick="javascript:location.href='localhost/shop/list.php'">Submit</button>
-                 </form>
-            </div>
+  <div class="navbar-wrapper">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar justified"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </div>
+      </button>
+      <form class="navbar-form navbar-right" role="search">
+       <div class="form-group">
+         <input type="text" class="form-control" placeholder="Search">
+       </div>
+       <button type="submit" class="btn btn-default" onclick="javascript:location.href='localhost/shop/list.php'">Submit</button>
+     </form>
+   </div>
 
-      </div>
-      <ul class="nav nav-pills nav-justified">
-    <li role="presentation" class="js-scroll-trigger"><a href="#introduction">소개</a></li>
-      <li role="presentation" class="js-scroll-trigger"><a  href="#list">상품목록</a></li>
-      <li role="presentation" class="js-scroll-trigger"><a href="#location">오시는 길</a></li>
-    </ul>
+ </div>
+ <ul class="nav nav-pills nav-justified">
+  <li role="presentation" class="js-scroll-trigger"><a href="#introduction">소개</a></li>
+  <li role="presentation" class="js-scroll-trigger"><a  href="#list">상품목록</a></li>
+  <li role="presentation" class="js-scroll-trigger"><a href="#location">오시는 길</a></li>
+</ul>
 
 
-    <style>
-    body { padding-bottom: 70px; }
-    </style>
-    <div class="vertical">
-    <span class="vertical">
+<style>
+body { padding-bottom: 70px; }
+</style>
+<div class="vertical">
+  <span class="vertical">
     건어물집 소개
-    </span>
-  </div>
-    <section id="introduction" class="jumbotron">
-       <div class="container">
-     <div class="row featurette">
-            <div class="col-md-7 col-md-push-5">
-              <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5 col-md-pull-7">
-              <img class="featurette-image img-responsive center-block" src="img/squid.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-            </div>
-          </div>
+  </span>
+</div>
+<section id="introduction" class="jumbotron">
+ <div class="container">
+   <div class="row featurette">
+    <div class="col-md-7 col-md-push-5">
+      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">
+        <!-- 소개 문구 -->
+        <?=$src2?>
+      </p>
     </div>
+    <div class="col-md-5 col-md-pull-7">
+      <img class="featurette-image img-responsive center-block" src="<?=$src?>" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    </div>
+  </div>
+</div>
+</section>
+<hr class="featurette-divider">
+
+<section id="list" class="container">
+  <div class="row">
+    <div class="col-lg-3">
+      <a href="<?php
+      echo "list.php?id=";
+      $temp = 2;
+      echo $temp;
+      ?>" class="thumbnail">
+      <img src="img/squid.jpg" alt="..."></a>
+      <!-- 여기서 디비에서 값이랑 아이디 가져오는거 추가하고 가져오면서 img 소스에 들어갈  -->
+      <!-- php 변수를 위에서 미리 정의해주자  -->
+    </div>
+    <div class="col-lg-3">
+      <a href="#" class="thumbnail">
+        <img src="img/squid.jpg" alt="..."></a>
+      </div>
+      <div class="col-lg-3">
+        <a href="#" class="thumbnail">
+          <img src="img/squid.jpg" alt="..."></a>
+        </div>
+      </div>
     </section>
-      <hr class="featurette-divider">
 
-    <section id="list" class="container">
-    <div class="row">
-          <div class="col-lg-3">
-            <a href="<?php
-            echo "list.php?id=";
-            $temp = 2;
-            echo $temp;
-            ?>" class="thumbnail">
-            <img src="img/squid.jpg" alt="..."></a>
-<!-- 여기서 디비에서 값이랑 아이디 가져오는거 추가하고 가져오면서 img 소스에 들어갈  -->
-<!-- php 변수를 위에서 미리 정의해주자  -->
-         </div>
-          <div class="col-lg-3">
-            <a href="#" class="thumbnail">
-          <img src="img/squid.jpg" alt="..."></a>
-           </div>
-          <div class="col-lg-3">
-            <a href="#" class="thumbnail">
-          <img src="img/squid.jpg" alt="..."></a>
-          </div>
+    <hr class="featurette-divider">
+    <section id="location" class="container">
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
-        </section>
-
-     <hr class="featurette-divider">
-     <section id="location" class="container">
-    <div class="row featurette">
-          <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5 col-md-pull-7">
-            <img class="featurette-image img-responsive center-block" src="img/squid.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-          </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="img/squid.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
         </div>
+      </div>
     </section>
     <hr class="featurette-divider">
 
@@ -205,14 +222,14 @@ $(function() {
         대표번호:02-333-3333 | 메일 : driedfood@naver.com
       </p>
       <p>
-        copyright (c)seafoodworld.com all rights reserved</p>
-      </footer>
-    </div>
-
+      copyright (c)seafoodworld.com all rights reserved</p>
+    </footer>
   </div>
-    <ul class="nav nav-pills nav-justified">
-        <li role="presentation"><a href="login.php">관리자모드</a></li>
-    </ul>
+
+</div>
+<ul class="nav nav-pills nav-justified">
+  <li role="presentation"><a href="login.php">관리자모드</a></li>
+</ul>
 
 
 
--- shop/manage_list.php
+++ shop/manage_list.php
@@ -14,12 +14,14 @@ include("config_getdata.php");
   <!-- 근데 세개가 row에 묶여있어도 조금이라도 창크기가 작아지면 -->
   <!-- 다 한줄에 하나만 옴 홈페이지에있는 템플릿도 이러함 -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>hi 건어물집에 오신걸 환영합니다</title>
+  <title>hi 건어물집에 오신걸 환영합니다</title>
   <link rel="stylesheet" href="css\bootstrap.css">
-    <link rel="stylesheet" href="css/box_design.css">
+  <link rel="stylesheet" href="css/box_design.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+
+
 <body>
   <div class="navbar-wrapper">
     <div class="container">
@@ -33,20 +35,20 @@ include("config_getdata.php");
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-    </div>
+          </div>
           <form class="navbar-form navbar-right" role="search">
-               <div class="form-group">
-                 <input type="text" class="form-control" placeholder="Search">
-               </div>
-               <button type="submit" class="btn btn-default" onclick="javascript:location.href='localhost/shop/list.php'">Submit</button>
-             </form>
-        </div>
-      </nav>
-    </div>
-  </div>
+           <div class="form-group">
+             <input type="text" class="form-control" placeholder="Search">
+           </div>
+           <button type="submit" class="btn btn-default" onclick="javascript:location.href='localhost/shop/list.php'">Submit</button>
+         </form>
+       </div>
+     </nav>
+   </div>
+ </div>
 
-<ul class="nav nav-pills nav-justified">
-<li role="presentation" class="active"><a href="#">소개</a></li>
+ <ul class="nav nav-pills nav-justified">
+  <li role="presentation" class="active"><a href="#">소개</a></li>
   <li role="presentation"><a href="#">상품목록</a></li>
   <li role="presentation"><a href="#">오시는 길</a></li>
 </ul>
@@ -55,97 +57,38 @@ body { padding-bottom: 70px; }
 </style>
 
 <div class="jumbotron">
-   <div class="container">
+ <div class="container">
    <h2>분류</h2>
 
-  <hr width="800">
-  <!-- 상품분류 선택이미지,페이지 누르면 링크로 redirect되게해야함 -->
-      <dl>
-<div class="container">
+   <hr width="800">
+   <!-- 상품분류 선택이미지,페이지 누르면 링크로 redirect되게해야함 -->
+   <dl>
+    <div class="container">
 
-       <div class="row" >
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
+     <div class="row" >
+       <div class="col-lg-2" >
+         <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
+         <strong>상품명</strong> 건조오징어
+         <br>
+         <strong>가격</strong> 만원
+         <form method="post" action="manage_goods.php">
           <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-           <br />
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
-          <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-      <br>
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
-          <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-       </div>
-       <br />
-       <div class="row">
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-           <br>
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
-          <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-        <br>
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
-          <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
- <!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-           <strong>상품명</strong> 건조오징어
-           <br>
-           <strong>가격</strong> 만원
-           <form method="post" action="manage_goods.php">
-          <button type="submit" class="btn btn-secondary btn-sm" >상품수정</button>
-          </form>
-          <form method="post" action="delete_goods.php">
-         <button type="submit" class="btn btn-secondary btn-sm">상품삭제</button></form>
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-       </div>
-     </dl>
-
+        </form>
+        <form method="post" action="delete_goods.php">
+         <button type="submit" class="btn btn-secondary btn-sm">상품삭제
+         </button>
+       </form>
+       <!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
+     </div>
    </div>
- </div>
-  </div>
-     <hr class="featurette-divider">
+
+   <br />
+ </dl>
+
+</div>
+</div>
+</div>
+<hr class="featurette-divider">
 
 
 </body>
