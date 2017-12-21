2d6231d7033a7d5238d15a2b22bdf887987d84ce
KSG <tomksg@naver.com>
2017-12-21 21:29:05 +0900

    list

--- shop/list.php
+++ shop/list.php
@@ -5,7 +5,7 @@
 include("config_getdata.php");
 
 // category값 01~06에 따라 다른 목록 업로드
-$category=$_GET[category];
+$category=$_GET['category'];
 
 ?>
 
@@ -19,10 +19,10 @@ $category=$_GET[category];
   <!-- 근데 세개가 row에 묶여있어도 조금이라도 창크기가 작아지면 -->
   <!-- 다 한줄에 하나만 옴 홈페이지에있는 템플릿도 이러함 -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>hi 건어물집에 오신걸 환영합니다</title>
+  <title>hi 건어물집에 오신걸 환영합니다</title>
   <link rel="stylesheet" href="css\bootstrap.css">
-    <link rel="stylesheet" href="css/box_design.css">
-    <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+  <link rel="stylesheet" href="css/box_design.css">
+  <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -42,135 +42,85 @@ $category=$_GET[category];
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-    </div>
+          </div>
           <form class="navbar-form navbar-right" role="search">
-               <div class="form-group">
-                 <input type="text" class="form-control" placeholder="Search">
-               </div>
-               <button type="submit" class="btn btn-default">Submit</button>
-             </form>
-        </div>
-      </nav>
-    </div>
-  </div>
+           <div class="form-group">
+             <input type="text" class="form-control" placeholder="Search">
+           </div>
+           <button type="submit" class="btn btn-default">Submit</button>
+         </form>
+       </div>
+     </nav>
+   </div>
+ </div>
 <!--
 <ul class="nav nav-pills nav-justified">
   <li role="presentation"><a href="list.php?categrory=1">반건조생선</a></li>
     <li role="presentation"><a href="list.php?categrory=2">건어물</a></li>
     <li role="presentation"><a href="list.php?categrory=3">젓갈</a></li>
     <li role="presentation"><a href="list.php?categrory=4">선물세트</a></li>
-</ul> -->
-<ul>
- <li><a href="list.php?categrory=1"><img src="img/lnav_1.png" alt="..." class="iist_image"></a></li>
- <li><a  href="list.php?categrory=2"><img src="img/lnav_2.png" alt="..." class="iist_image"></a></li>
- <li><a href="list.php?categrory=3"><img src="img/lnav_3.png" alt="..." class="iist_image"></a></li>
-  <li><a href="list.php?categrory=4"><img src="img/lnav_4.png" alt="..." class="iist_image"></a></li>
-</ul>
-&nbsp;
-<!-- 근데 창줄이면 깨지네 ;; -->
-<br>
+  </ul> -->
+  <ul>
+   <li><a href="list.php?category=1"><img src="img/lnav_1.png" alt="..." class="iist_image"></a></li>
+   <li><a  href="list.php?category=2"><img src="img/lnav_2.png" alt="..." class="iist_image"></a></li>
+   <li><a href="list.php?category=3"><img src="img/lnav_3.png" alt="..." class="iist_image"></a></li>
+   <li><a href="list.php?category=4"><img src="img/lnav_4.png" alt="..." class="iist_image"></a></li>
+ </ul>
+ &nbsp;
+ <!-- 근데 창줄이면 깨지네 ;; -->
+ <br>
 
-   <div class="container">
+ <div class="container">
    <h2>분류</h2>
-  <hr>
+   <hr>
 
-      <dl>
-
-
+   <dl>
 
 
-<div class="container">
+    <?php
+    $category=$_GET['category'];
+    $query = "SELECT * FROM gunfish WHERE category = '$category' ORDER BY product_id DESC";
+    $result = mysqli_query($bd, $query);
+    while($row=mysqli_fetch_array($result))
+    {
+      ?>
+
+      <div class="container">
 
        <div class="row" >
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-       </div>
-       <br />
-       <div class="row">
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-         <div class="col-lg-2" >
-           <a href="<?php
-            $temp_id = 2;
-           echo "goods.php?category=",$category,"?id=",$temp_id;
-           ?>">
-           <img src="img/squid.jpg" alt="Generic placeholder image" width="180" height="140">
-         </a>
-           <strong>상품명</strong> 건조오징어
-          <br>
-           <strong>가격</strong> 만원
-<!-- 우리가 원하는 모양으로 나타내주는 코드를 그냥 php내부에서 html로 만들어줘야할듯 -->
-         </div>
-       </div>
-     </dl>
 
-   </div>
- </div>
+         <div class="col-md-3" >
+          <img src="<?=$row['product_image']?>" alt="Generic placeholder image" width="180" height="140">
 
- <footer class="footer-distributed">
-   <p class="footer-links" >
-     <a href="login.php" style="color:#C6C4C3;">관리자모드</a>
-     &nbsp;
-     <a href="index.php" style="color:#C6C4C3;">홈페이지</a>
+          <strong>상품명 :</strong> <?=$row['product_name']?>
+          <br>
+          <strong>가격 :</strong> <?=$row['product_price']?> 원
 
-   </p>
-<hr />
+        </div>
+      </div>
+      <br />
+      <?php
+    // $no = $no - 1;
+    }
+    mysqli_close($bd);
+    ?>
+  </div>
+
+  <br />
+
+</dl>
+
+</div>
+</div>
+
+<footer class="footer-distributed">
+ <p class="footer-links" >
+   <a href="login.php" style="color:#C6C4C3;">관리자모드</a>
+   &nbsp;
+   <a href="index.php" style="color:#C6C4C3;">홈페이지</a>
+
+ </p>
+ <hr />
  주식회사 건어물집  | 사업자등록번호 : 123-45-67890 | 대표이사 : 김건어물
  <br>
  주소 : 경기도 고양시 덕양구 항공대학로 76 | 개인정보관리책임 : 김보안 | 이메일 : driedfood@naver.com
@@ -178,7 +128,7 @@ $category=$_GET[category];
  고객센터 : 02-333-3333 | FAX : 02-333-3333
  <br>
  COPYRIGHT © 2017 DRIED FOOD SHOP ALL RIGHTS RESERVED
- </footer>
+</footer>
 
 
 </body>
