086de314d21e0506ffe4be1eaabb20bb05dc3f2c
JinHeeeKang <wlsgml9975@naver.com>
2017-12-22 02:09:57 +0900

    .
    .

--- shop/index.php
+++ shop/index.php
@@ -177,8 +177,9 @@ $row2 = mysqli_fetch_array($result2);
   <img src="img/nav_2.png" alt="...">
     <img src="img/nav_3.png" alt="..."> -->
 
+<br><br>
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width:100%">
       <!-- Indicators -->
       <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -191,8 +192,7 @@ $row2 = mysqli_fetch_array($result2);
           <div class="container">
             <div class="carousel-caption">
               <h1>Example headline.</h1>
-              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              <p></p>
             </div>
           </div>
         </div>
@@ -202,7 +202,7 @@ $row2 = mysqli_fetch_array($result2);
             <div class="carousel-caption">
               <h1>Another example headline.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              <p></p>
             </div>
           </div>
         </div>
@@ -212,7 +212,7 @@ $row2 = mysqli_fetch_array($result2);
             <div class="carousel-caption">
               <h1>One more for good measure.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              <p></p>
             </div>
           </div>
         </div>
