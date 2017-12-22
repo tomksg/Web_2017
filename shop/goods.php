27fc5ce28a20b893df825c0eb5e980a0734beb1e
KSG <tomksg@naver.com>
2017-12-22 12:02:46 +0900

    goods

--- shop/goods.php
+++ shop/goods.php
@@ -18,6 +18,7 @@ $product_id=$_GET['product_id'];
 $result = mysqli_query($bd, "SELECT * FROM gunfish WHERE product_id = '$product_id'");
 $row=mysqli_fetch_array($result);
 
+$category=$row['category'];
 
 // echo $row['product_id'];
 // echo '<br>';
@@ -49,64 +50,82 @@ $row=mysqli_fetch_array($result);
   </div>
 
 
-               <ul class="nav  nav-justified navbar-default" id="colored">
-             <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=1">반건조생선</a></li>
-               <li role="presentation" class="js-scroll-trigger" id="bar"><a  href="list.php?category=2">건어물</a></li>
-               <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=3">젓갈</a></li>
-               <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=4g">선물세트</a></li>
-             </ul>
-  <hr class="featurette-divider">
-  <div class="container">
+  <ul class="nav  nav-justified navbar-default" id="colored">
+   <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=1">반건조생선</a></li>
+   <li role="presentation" class="js-scroll-trigger" id="bar"><a  href="list.php?category=2">건어물</a></li>
+   <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=3">젓갈</a></li>
+   <li role="presentation" class="js-scroll-trigger" id="bar"><a href="list.php?category=4g">선물세트</a></li>
+ </ul>
+ <hr class="featurette-divider">
+ <div class="container">
 
-    <h2>분류</h2>
-    <hr>
-    <dl class="row">
-      <div class="col-lg-3" >
-        <img src="<?=$row['product_image']?>" alt="Generic placeholder image" width="250" height="250">
-      </div>
-      <div class="col-lg-3">
-        <strong>상품명 :<strong><?=$row['product_name']?>
-          <hr />
-          <p>
-            <strong>가격 :</strong> <?=$row['product_price']?> 원
-          </p>
-          <p>
-            <strong>제품특징 :</strong> <?=$row['product_info']?>
-          </p>
-          <p>
-            <strong>원산지</strong><?=$row['product_from']?>
-          </p>
-          <p>
-            <strong>중량</strong><?=$row['product_weight']?>
-          </p>
-          <p>
-            <strong>소개</strong><?=$row['product_intro']?>
-          </p>
-
-        </div>
-      </dl>
-
+   <h1><script >
+    var category = '<?=$category?>'
+    switch(category){
+      case '1':
+      document.writeln('반건조생선');
+      break;
+      case '2':
+      document.writeln('건어물');
+      break;
+      case '3':
+      document.writeln('젓갈');
+      break;
+      case '4':
+      document.writeln('선물세트');
+      break;
+      default:
+      break;
+    }
+  </script></h1>
+  <hr>
+  <dl class="row">
+    <div class="col-lg-3" >
+      <img src="<?=$row['product_image']?>" alt="Generic placeholder image" width="250" height="250">
     </div>
+    <div class="col-lg-3">
+      <strong>상품명 :<strong><?=$row['product_name']?>
+        <hr />
+        <p>
+          <strong>가격 :</strong> <?=$row['product_price']?> 원
+        </p>
+        <p>
+          <strong>제품특징 :</strong> <?=$row['product_info']?>
+        </p>
+        <p>
+          <strong>원산지</strong><?=$row['product_from']?>
+        </p>
+        <p>
+          <strong>중량</strong><?=$row['product_weight']?>
+        </p>
+        <p>
+          <strong>소개</strong><?=$row['product_intro']?>
+        </p>
 
-    <footer class="footer-distributed">
-     <p class="footer-links" >
-       <a href="login.php" style="color:#C6C4C3;">관리자모드</a>
-       &nbsp;
-       <a href="index.php" style="color:#C6C4C3;">홈페이지</a>
+      </div>
+    </dl>
 
-     </p>
-     <hr />
-     주식회사 건어물집  | 사업자등록번호 : 123-45-67890 | 대표이사 : 김건어물
-     <br>
-     주소 : 경기도 고양시 덕양구 항공대학로 76 | 개인정보관리책임 : 김보안 | 이메일 : driedfood@naver.com
-     <br>
-     고객센터 : 02-333-3333 | FAX : 02-333-3333
-     <br>
-     COPYRIGHT © 2017 DRIED FOOD SHOP ALL RIGHTS RESERVED
-   </footer>
+  </div>
+
+  <footer class="footer-distributed">
+   <p class="footer-links" >
+     <a href="login.php" style="color:#C6C4C3;">관리자모드</a>
+     &nbsp;
+     <a href="index.php" style="color:#C6C4C3;">홈페이지</a>
+
+   </p>
+   <hr />
+   주식회사 건어물집  | 사업자등록번호 : 123-45-67890 | 대표이사 : 김건어물
+   <br>
+   주소 : 경기도 고양시 덕양구 항공대학로 76 | 개인정보관리책임 : 김보안 | 이메일 : driedfood@naver.com
+   <br>
+   고객센터 : 02-333-3333 | FAX : 02-333-3333
+   <br>
+   COPYRIGHT © 2017 DRIED FOOD SHOP ALL RIGHTS RESERVED
+ </footer>
 
 
 
 
- </body>
- </html>
+</body>
+</html>
